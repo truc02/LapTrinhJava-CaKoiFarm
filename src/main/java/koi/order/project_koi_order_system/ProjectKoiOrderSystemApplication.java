6525fef996f0bd968c036c5a34048bf116a160e1
truc02 <nguyenminhtruc29022004@gmail.com>
2024-10-14 01:59:01 +0700

    Truc commit

---
+++ src/main/java/koi/order/project_koi_order_system/ProjectKoiOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package koi.order.project_koi_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKoiOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectKoiOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/koi/order/project_koi_order_system/ProjectKoiOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package koi.order.project_koi_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKoiOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
