c63280930cd1f5a16601cde16382f8cfc8710121
truc02 <nguyenminhtruc29022004@gmail.com>
2024-10-15 00:51:42 +0700

    CurT_commit

--- src/main/java/koi/classes/User/User.java
+++ src/main/java/koi/classes/User/User.java
@@ -1,4 +1,51 @@
 package koi.classes.User;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "fullname")
+    private String fullname;
+
+    @Column(name = "birthday")
+    private String birthday;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public User(String username, String fullname, String birthday, String password, String image, String address, String phone, String email) {
+        this.username = username;
+        this.fullname = fullname;
+        this.birthday = birthday;
+        this.password = password;
+        this.image = image;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
 }
---
+++ src/main/java/koi/classes/gui/projectMain.java
@@ -0,0 +1,9 @@
+package koi.classes.gui;
+
+import koi.classes.User.User;
+
+public class projectMain {
+    public static void main(String[] args) {
+        User user = new User("truc","trucNguyen","29/02/2004","12345","1231","Nhon Loc","0387368199","2251120192@ut.edu.vn");
+    }
+}
